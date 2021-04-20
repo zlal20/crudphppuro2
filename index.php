121e1be48d6755b4251bdd0502f7ed3d7a2b3403
Eduardo Franco <jesuseduardofrancomejia69@gmail.com>
2021-04-19 22:24:05 -0500

    bibliotrca

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Administrar Libro</title>
+	</head>	
+	<body>
+		<header>
+			Bienvenido Administrar Libro
+		</header>
+		<table border=1>			
+			<tr>
+				<td><a href="ingreso.php">Ingresar</a></td>
+			</tr>
+			<tr>
+				<td><a href="mostrar.php">Ver</a></td>
+			</tr>
+		</table>
+		<footer>
+			Administrar Libro 2017
+		</footer>
+	</body>
+</html>
---
+++ libro.php
@@ -0,0 +1,41 @@
+<?php
+	class Libro{
+		private $id;
+		private $nombre;
+		private $autor;
+		private $anio_edicion;
+
+		function __construct(){}
+
+		public function getNombre(){
+		return $this->nombre;
+		}
+
+		public function setNombre($nombre){
+			$this->nombre = $nombre;
+		}
+
+		public function getAutor(){
+			return $this->autor;
+		}
+
+		public function setAutor($autor){
+			$this->autor = $autor;
+		}
+
+		public function getAnio_edicion(){
+		return $this->anio_edicion;
+		}
+
+		public function setAnio_edicion($anio_edicion){
+			$this->anio_edicion = $anio_edicion;
+		}
+		public function getId(){
+			return $this->id;
+		}
+
+		public function setId($id){
+			$this->id = $id;
+		}
+	}
+?>
